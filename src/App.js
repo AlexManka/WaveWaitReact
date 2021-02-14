d4838efbe8c0a9a1a875b828f8eeb1a1c868b77b
Alex Manka <alexmanka2022@u.northwestern.edu>
2021-02-14 01:05:11 -0600

    Add files via upload
    Might be missing node modules? Google login works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import NewEvent from './components/NewEvent/NewEvent';
+import Login from './components/Login/Login';
+import Logout from './components/Login/Logout';
 
 function App() {
   return (
     <div className="App">
+      <Login />
+      <Logout />
       <NewEvent />
     </div>
   );
